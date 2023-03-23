493931ee2fa659254ff745e598d526230a382339
vin <didekqls93@gmail.com>
2023-03-23 13:40:12 +0900

    Error : 숫자 +숫자+ 문자열 = 문자열 출력

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-function add(n1, n2) {
-    return n1 + n2;
+// function add(n1: number, n2: number) {
+//   return n1 + n2;
+// }
+// var number1 = 5;
+// var number2 = 2.8;
+// var result = add(number1, number2);
+// console.log(result);
+// //위에서는 number type의 합을 보여주기때문에 result= 7.8 이 나오지만
+// var string = "5";
+// var result = add(string, number2);
+// console.log(result);
+// // string + number type의 합이기 때문에 52.8로 return 하게된다
+// const number3 = "6";
+// const result2 = add(number1, number3);
+// console.log(result2);
+function add(n1, n2, showResult, phrase) {
+    if (showResult) {
+        console.log(phrase + n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
+var printResult = true;
+var resultPhrase = "Result is: ";
+var result = add(number1, number2, printResult, resultPhrase);
 console.log(result);
-//위에서는 number type의 합을 보여주기때문에 result= 7.8 이 나오지만
-var string = "5";
-var result = add(string, number2);
-console.log(result);
-// string + number type의 합이기 때문에 52.8로 return 하게된다
-var number3 = "6";
-var result2 = add(number1, number3);
-console.log(result2);
--- app.ts
+++ app.ts
@@ -1,20 +1,37 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+// function add(n1: number, n2: number) {
+//   return n1 + n2;
+// }
+// var number1 = 5;
+// var number2 = 2.8;
+// var result = add(number1, number2);
+// console.log(result);
+
+// //위에서는 number type의 합을 보여주기때문에 result= 7.8 이 나오지만
+
+// var string = "5";
+// var result = add(string, number2);
+// console.log(result);
+
+// // string + number type의 합이기 때문에 52.8로 return 하게된다
+
+// const number3 = "6";
+
+// const result2 = add(number1, number3);
+// console.log(result2);
+
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  if (showResult) {
+    console.log(phrase + n1 + n2);
+  } else {
+    return n1 + n2;
+  }
 }
-var number1 = 5;
-var number2 = 2.8;
-var result = add(number1, number2);
+
+const number1 = 5;
+
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+const result = add(number1, number2, printResult, resultPhrase);
 console.log(result);
-
-//위에서는 number type의 합을 보여주기때문에 result= 7.8 이 나오지만
-
-var string = "5";
-var result = add(string, number2);
-console.log(result);
-
-// string + number type의 합이기 때문에 52.8로 return 하게된다
-
-const number3 = "6";
-
-const result2 = add(number1, number3);
-console.log(result2);
