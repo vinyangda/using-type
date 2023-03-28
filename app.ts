1ea7e4ff2960b9518027294bf42ac0c586c129a0
vin <didekqls93@gmail.com>
2023-03-29 00:34:57 +0900

    array type

--- app.ts
+++ app.ts
@@ -1,11 +1,16 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+const person = {
   name: "Dabin",
   age: 30,
+  hobbies: ["Game", "Drawing", "Play Guitar"],
 };
+
+let favoriteActivities: string[];
+favoriteActivities = ["Game"];
 
 console.log(person.name);
 
-//중첩 된 객체 및 타입;
+// 여기까지 중첩 된 객체 및 타입;
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
